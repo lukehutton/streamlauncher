ebd40cda51b1f32308914e13a8d47602787b74d6
Luke Hutton <lukehutton@gmail.com>
2015-01-09 13:03:55 -0800

    Add ignore configs scripts

---
+++ scripts/ignore_configs.sh
@@ -0,0 +1,4 @@
+ #!/bin/bash  
+ 
+git update-index --assume-unchanged ../StreamLauncher.Wpf/App.config
+git update-index --assume-unchanged ../StreamLauncher.Tests.Integration/App.config
---
+++ scripts/undo_ignore_configs.sh
@@ -0,0 +1,4 @@
+ #!/bin/bash  
+ 
+git update-index --no-assume-unchanged ../StreamLauncher.Wpf/App.config
+git update-index --no-assume-unchanged ../StreamLauncher.Tests.Integration/App.config
