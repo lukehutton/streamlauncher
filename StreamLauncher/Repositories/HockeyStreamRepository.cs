cb605c253e243e0a6ae567b27128d1a90c1039f7
Luke Hutton <lukehutton@gmail.com>
2015-01-26 20:11:56 -0800

    Add logging to streams repo

--- StreamLauncher/Repositories/HockeyStreamRepository.cs
+++ StreamLauncher/Repositories/HockeyStreamRepository.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using log4net;
 using RestSharp;
 using StreamLauncher.Api;
 using StreamLauncher.Constants;
@@ -12,13 +14,13 @@ using StreamLauncher.Models;
 using StreamLauncher.Util;
 
 namespace StreamLauncher.Repositories
-{
-    // todo unit test
+{    
     public class HockeyStreamRepository : IHockeyStreamRepository
     {
         private readonly ILiveStreamScheduleAggregatorAndMapper _aggregatorAndMapper;
         private readonly IHockeyStreamsApiRequiringToken _hockeyStreamsApi;
         private readonly IScoresRepository _scoresRepository;
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
         public HockeyStreamRepository(
             IHockeyStreamsApiRequiringToken hockeyStreamsApi,
@@ -34,8 +36,18 @@ namespace StreamLauncher.Repositories
         public Task<IEnumerable<HockeyStream>> GetLiveStreams(DateTime date)
         {
             var request = new RestRequest {Resource = "GetLive", Method = Method.GET};
-            request.AddParameter("date", date.ToString("MM/dd/yyyy"), ParameterType.GetOrPost);
+            var dateParameter = new Parameter
+            {
+                Name = "date",
+                Value = date.ToString("MM/dd/yyyy"),
+                Type = ParameterType.GetOrPost
+            };
+            request.AddParameter(dateParameter);
+            
+            Log.InfoFormat("Retrieving live streams with parameter: {0}-{1}", dateParameter.Name, dateParameter.Value);
             var responseDto = _hockeyStreamsApi.Execute<GetLiveStreamsResponseDto>(request);
+            Log.InfoFormat("Retrieved {0} live streams", responseDto.Schedule.Count);
+
             var hockeyStreams = _aggregatorAndMapper.AggregateAndMap(responseDto);
             var scores = _scoresRepository.GetScores();
             var streamsWithScores = hockeyStreams.Select(stream =>
@@ -57,9 +69,26 @@ namespace StreamLauncher.Repositories
         public Task<LiveStream> GetLiveStream(int streamId, string location, Quality quality)
         {
             var request = new RestRequest {Resource = "GetLiveStream", Method = Method.GET};
-            request.AddParameter("id", streamId, ParameterType.GetOrPost);
-            request.AddParameter("location", location, ParameterType.GetOrPost);
-            var responseDto = _hockeyStreamsApi.Execute<GetLiveStreamResponseDto>(request);
+            var idParameter = new Parameter
+            {
+                Name = "id",
+                Value = streamId,
+                Type = ParameterType.GetOrPost
+            };
+            var locationParameter = new Parameter
+            {
+                Name = "location",
+                Value = location,
+                Type = ParameterType.GetOrPost
+            };
+            request.AddParameter(idParameter);
+            request.AddParameter(locationParameter);
+            
+            Log.InfoFormat("Retrieving live stream with parameters: {0}-{1}, {2}-{3}",
+                idParameter.Name, idParameter.Value, locationParameter.Name, locationParameter.Value
+                );
+            var responseDto = _hockeyStreamsApi.Execute<GetLiveStreamResponseDto>(request);            
+
             var liveStream = new LiveStream();
             switch (quality)
             {
@@ -69,6 +98,7 @@ namespace StreamLauncher.Repositories
                         StreamNotFoundException(streamId, location, quality);
                     }
                     liveStream.Source = responseDto.TrueLiveSD.First().Src;
+                    Log.InfoFormat("Retrieved SD live stream with source: {0}", liveStream.Source);
                     break;
                 case Quality.HD:
                     if (responseDto.TrueLiveHD.Count == 0)
@@ -76,16 +106,19 @@ namespace StreamLauncher.Repositories
                         StreamNotFoundException(streamId, location, quality);
                     }
                     liveStream.Source = responseDto.TrueLiveHD.First().Src;
+                    Log.InfoFormat("Retrieved HD live stream with source: {0}", liveStream.Source);
                     break;
             }
             return Task.FromResult(liveStream);
         }
 
-        private static void StreamNotFoundException(int streamId, string location, Quality quality)
-        {
-            throw new StreamNotFoundException(
+        private void StreamNotFoundException(int streamId, string location, Quality quality)
+        {            
+            var exception = new StreamNotFoundException(
                 string.Format("StreamId: {0}, Location: {1}, Quality: {2} Not Found", streamId, location,
                     quality));
+            Log.Error(exception.Message, exception);
+            throw exception;
         }
 
         private static void DeterminePeriodAndScore(HockeyStream stream)
